7965ce76ed877b02d1df0651c0a5f3137c0a9f91
Dr-Deep <80998605+Dr-Deep@users.noreply.github.com>
2024-08-30 20:47:16 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,6 +29,8 @@
 
 ;; <ESC> quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-a") 'mark-whole-buffer)
+
 
 ;; Transparency
 (defvar efs/frame-transparency '(90 . 90))
@@ -62,23 +64,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-
-;; Straight
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
 
 ;;
 ;; END PACKAGE SOURCES END
